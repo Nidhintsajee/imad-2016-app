f948a364b81177c503efea399960f983e0cb4e2e
Nidhin <nidhintsajee@gmail.com>
2016-09-24 12:59:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML='New value';
 //move the image
 var img=document.getElementById('madi');
 var marginLeft=0;
-function moveRoight(){
+function moveRight(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
 }
